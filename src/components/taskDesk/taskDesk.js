2fd349f41fc2ada061a21c2fde7c8ea73837865b
Rafael <rafael_guliev94@list.ru>
2019-10-10 00:11:23 +0300

    add dragAndDrop function (not complete)

--- src/components/taskDesk/taskDesk.js
+++ src/components/taskDesk/taskDesk.js
@@ -2,35 +2,32 @@ export default function taskDesk() {
     const deskWrapper = document.querySelector('.desk__wrapper');
     const temp = document.querySelector('.js-template');
     const deskClone = temp.content.cloneNode(true);
+    const dragElement = document.querySelector('.js-drag-element');
 
     class Kanban {
         constructor() {
             this.element = temp.content.cloneNode(true);
             const element = this.element.querySelector('.taskDesk');
-
-            const taskDesk = element.querySelector('.taskDesk');
-            const taskAddBox = element.querySelector('.taskDesk_add_box');
             const buttonSave = element.querySelector('.taskDesk_button_save');
             const buttonAddTitle = element.querySelector('.js-add-column');
             const buttonSaveTitle = element.querySelector('.js-save-title');
 
             const taskArea = element.querySelector('.js-task-area');
             const taskInput = element.querySelector('.js-task-input');
-            const taskList = element.querySelector('.task_list');
 
             const closeCardBottom = element.querySelector('.js-close');
             const openCardBottom = element.querySelector('.js-add-card');
 
-
-
             buttonAddTitle.addEventListener('click', this.handleAddTitle.bind(element));
             buttonSaveTitle.addEventListener('click', this.handleSaveTitle.bind(element));
+
             taskInput.addEventListener('input', this.handleCheckTitle.bind(element));
             taskArea.addEventListener('input', this.handleCheckArea.bind(element));
+
             buttonSave.addEventListener('click', this.handleSaveTask.bind(element));
+
             closeCardBottom.addEventListener('click', this.handleCloseBottomMenu.bind(element));
             openCardBottom.addEventListener('click', this.handleOpenBottomMenu.bind(element));
-
         }
 
         handleAddTitle() {
@@ -62,8 +59,6 @@ export default function taskDesk() {
 
             taskAddBox.classList.remove('addColumn');
             taskAddBox.classList.add('active');
-
-
             new Kanban().appendDesk();
         }
 
@@ -89,9 +84,13 @@ export default function taskDesk() {
             newTask.innerHTML = taskArea.value;
             taskList.appendChild(newTask);
 
+            dragAndDrop(newTask);
+
+
             taskArea.value = '';
             taskAddBox.classList.add('emptyArea');
         }
+
 
         handleCloseBottomMenu() {
             const taskAddBox = this.querySelector('.taskDesk_add_box');
@@ -106,6 +105,61 @@ export default function taskDesk() {
         appendDesk() {
             deskWrapper.appendChild(this.element);
         }
+    }
+
+    function dragAndDrop(elem) {
+        elem.addEventListener('mousedown', function (e) {
+            e.preventDefault();
+            let startCoords = {
+                x: e.clientX,
+                y: e.clientY
+            };
+            console.log(startCoords.x, startCoords.y);
+
+            dragElement.style.top = elem.getBoundingClientRect().top + 'px';
+            dragElement.style.left = elem.getBoundingClientRect().left + 'px';
+
+            function onMouseMove(moveEvt) {
+                moveEvt.preventDefault();
+
+                let shift = {
+                    x: startCoords.x - moveEvt.clientX,
+                    y: startCoords.y - moveEvt.clientY
+                };
+
+                startCoords = {
+                    x: moveEvt.clientX,
+                    y: moveEvt.clientY
+                };
+
+                dragElement.innerHTML = elem.innerHTML;
+                dragElement.style.display = 'block';
+                dragElement.style.zIndex = 1000;
+
+                elem.classList.add('ondrag');
+
+                dragElement.style.top = (dragElement.offsetTop - shift.y) + 'px';
+                dragElement.style.left = (dragElement.offsetLeft - shift.x) + 'px';
+                document.body.style.cursor = 'move';
+
+                console.log(document.elementFromPoint(moveEvt.x, moveEvt.y));
+            };
+
+            const onMouseUp = function (upEvt) {
+                upEvt.preventDefault();
+                document.removeEventListener('mousemove', onMouseMove);
+                document.removeEventListener('mouseup', onMouseUp);
+
+
+                elem.classList.remove('ondrag');
+                dragElement.style.display = 'none';
+                document.body.style.cursor = 'auto';
+
+            };
+
+            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mouseup', onMouseUp);
+        })
     }
 
     const newDesk = new Kanban();
